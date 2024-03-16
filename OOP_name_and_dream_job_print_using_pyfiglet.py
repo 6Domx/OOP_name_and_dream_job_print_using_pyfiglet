c6ccbfe0152aa9f925f8bfbf5914a7c98b8b0500
Miguel Dominic Payumo <150775008+6Domx@users.noreply.github.com>
2024-03-16 21:50:13 +0800

    name and job input

--- OOP_name_and_dream_job_print_using_pyfiglet.py
+++ OOP_name_and_dream_job_print_using_pyfiglet.py
@@ -3,3 +3,9 @@
 # 2. It should print your name
 # 3. It should print your dream job
 # 4. Use fancy text designs
+
+import pyfiglet
+
+your_name = input("What is your name?: ")
+dream_job = input("What is your dream job?: ")
+
